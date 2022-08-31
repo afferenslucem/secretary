92052884fb3fd011d058acd9d480f0b6ab3ac5f5
Alex Shakirov <alexshakirov74@gmail.com>
2022-08-31 22:33:15 +0500

    added command clip

---
+++ secretary.telegram.tests/commands/CommandClipTests.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using secretary.logging;
+using secretary.telegram.commands;
+using secretary.telegram.exceptions;
+using secretary.telegram.sessions;
+
+namespace secretary.telegram.tests.commands;
+
+public class CommandClipTests
+{
+    private ILogger<CommandClipTests> _logger = LogPoint.GetLogger<CommandClipTests>();
+    
+    private CommandClip _clip = null!;
+
+    private Mock<Command> _firstState = null!; 
+    private Mock<Command> _secondState = null!; 
+    private Mock<Command> _thirdState = null!; 
+    private Mock<Command> _command = null!;
+
+    private CommandContext _context = new CommandContext();
+
+    [SetUp]
+    public void Setup()
+    {
+        _firstState = new(); 
+        _secondState = new(); 
+        _thirdState = new(); 
+        _command = new(); 
+        
+        _clip = new(
+            new List<Command>() {_firstState.Object, _secondState.Object, _thirdState.Object},
+            _command.Object
+            );
+    }
+
+    [Test]
+    public void ShouldCreate()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public async Task ShouldRunFirstState()
+    {
+        await _clip.Run(_context);
+        
+        _firstState.Verify(target => target.ValidateMessage(), Times.Once);
+        _firstState.Verify(target => target.OnMessage(), Times.Once);
+        
+        Assert.That(_clip.RunIndex, Is.EqualTo(1));
+        
+        _secondState.Verify(target => target.Execute(), Times.Once);
+    }
+
+    [Test]
+    public async Task ShouldRunSecondState()
+    {
+        await _clip.Run(_context);
+        await _clip.Run(_context);
+        
+        _secondState.Verify(target => target.ValidateMessage(), Times.Once);
+        _secondState.Verify(target => target.OnMessage(), Times.Once);
+        
+        Assert.That(_clip.RunIndex, Is.EqualTo(2));
+        
+        _thirdState.Verify(target => target.Execute(), Times.Once);
+    }
+
+    [Test]
+    public async Task ShouldIgnoreExecuteOnLastStep()
+    {
+        await _clip.Run(_context);
+        await _clip.Run(_context);
+        await _clip.Run(_context);
+        
+        _thirdState.Verify(target => target.ValidateMessage(), Times.Once);
+        _thirdState.Verify(target => target.OnMessage(), Times.Once);
+        
+        Assert.That(_clip.RunIndex, Is.EqualTo(3));
+    }
+
+    [Test]
+    public async Task ShouldIgnoreStepIncrementForValidationError()
+    {
+        await _clip.Run(_context);
+        
+        _secondState.Setup(target => target.ValidateMessage()).ThrowsAsync(new IncorrectFormatException());
+        
+        Assert.That(_clip.RunIndex, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task ShouldCancelCurrentChild()
+    {
+        await _clip.Run(_context);
+        await _clip.Cancel(_context);
+        
+        _secondState.Verify(target => target.Cancel(), Times.Once);
+    }
+}
---
+++ secretary.telegram/commands/CommandClip.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using secretary.logging;
+using secretary.telegram.commands.executors;
+using secretary.telegram.exceptions;
+
+namespace secretary.telegram.commands;
+
+public class CommandClip
+{
+    private readonly ILogger<CommandClip> _logger = LogPoint.GetLogger<CommandClip>();
+
+    private readonly Command _parentCommand;
+    private readonly Command[] _states;
+    private int _runIndex = 0;
+
+    public int RunIndex => _runIndex;
+
+    public CommandClip(IEnumerable<Command> states, Command parentCommand)
+    {
+        _parentCommand = parentCommand;
+        _states = states.ToArray();
+    }
+
+    public async Task Run(CommandContext context)
+    {
+        var firstPartCommand = _states[_runIndex];
+            
+        try
+        {
+            var firstPartExecutor = new ChildCommandExecutor(firstPartCommand, context, _parentCommand);
+
+            await firstPartExecutor.ValidateMessage();
+            await firstPartExecutor.OnMessage();
+
+            _runIndex++;
+
+            if (_runIndex == this._states.Length) return;
+
+            var secondPartCommand = _states[_runIndex];
+            var secondPartExecutor = new ChildCommandExecutor(secondPartCommand, context, _parentCommand);
+
+            await secondPartExecutor.Execute();
+        }
+        catch (IncorrectFormatException e)
+        {
+            _logger.LogWarning(e, $"Некорректный формат команды {firstPartCommand.GetType().Name}: \"{context.Message}\"");
+        }
+    }
+
+    public Task Cancel(CommandContext context)
+    {
+        var state = _states[_runIndex];
+        var executor = new ChildCommandExecutor(state, context, _parentCommand);
+
+        return executor.Cancel();
+    }
+}
