983082a0d3b0f6ab7aea573a1e4dee3b8a51c5f9
Alex Shakirov <alexshakirov74@gmail.com>
2022-09-17 10:39:39 +0500

    changed backup script

--- scripts/secretary_db_dump.sh
+++ scripts/secretary_db_dump.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
-pathDB=?
-pathBackup=?
+PGPASSWORD=sc@ry_passwo0rd
+export PGPASSWORD
+pathB=/root/Yandex.Disk/backups/secretary/database/
+dbUser=secretary
+database=secretary
 
-cp $pathDB/database.sqlite $pathBackup/database-$(date "+%Y-%m-%dT%H:%M").sqlite
+pg_dump -U $dbUser -d $database -h localhost -p 35432 | gzip > $pathB/database-$(date "+%Y-%m-%dT%H:%M").bak.gzip
+
+unset PGPASSWORD
