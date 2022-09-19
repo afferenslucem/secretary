f802e070fa66b5770da871a398f2ec92179594f3
Alex Shakirov <alexshakirov74@gmail.com>
2022-09-19 23:19:41 +0500

    bump version

--- Secretary.Telegram/TelegramBot.cs
+++ Secretary.Telegram/TelegramBot.cs
@@ -16,7 +16,7 @@ namespace Secretary.Telegram;
 
 public class TelegramBot
 {
-    public static readonly string Version = "v2.3.0";
+    public static readonly string Version = "v2.4.0";
     
     public static readonly DateTime Uptime = DateTime.Now;
     
