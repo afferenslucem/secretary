5c9fd641d1b08797042b52607f49e564228725c4
Alex Shakirov <a.shakirov@infinnity.ru>
2022-09-20 09:33:59 +0500

    fixed tests

--- Secretary.Frontend/projects/panel/src/app/pages/statistic-page/resolvers/statistic.resolver.spec.ts
+++ Secretary.Frontend/projects/panel/src/app/pages/statistic-page/resolvers/statistic.resolver.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
 import { StatisticResolver } from './statistic.resolver';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe('StatisticResolver', () => {
   let resolver: StatisticResolver;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     resolver = TestBed.inject(StatisticResolver);
   });
 
--- Secretary.Frontend/projects/panel/src/app/pages/statistic-page/statistic-page.component.spec.ts
+++ Secretary.Frontend/projects/panel/src/app/pages/statistic-page/statistic-page.component.spec.ts
@@ -1,14 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StatisticPageComponent } from './statistic-page.component';
+import { RouterTestingModule } from "@angular/router/testing";
+import { ActivatedRoute } from "@angular/router";
+import { Statistic } from "./models/i-statistic";
 
 describe('StatisticPageComponent', () => {
   let component: StatisticPageComponent;
   let fixture: ComponentFixture<StatisticPageComponent>;
 
   beforeEach(async () => {
+    const statistic: Statistic = {
+      documentStatistic: { timeOffCount: 0, vacationCount: 0, distantCount: 0 },
+      userStatistic: { totalUsers: 0, userWithDocuments: 0 },
+    }
+
     await TestBed.configureTestingModule({
-      imports: [ StatisticPageComponent ]
+      imports: [ StatisticPageComponent ],
+      providers: [
+        RouterTestingModule,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                statistic
+              }
+            }
+          }
+        }
+      ]
     })
     .compileComponents();
 
